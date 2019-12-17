51e82e8799c54e9d8d43c6aab71cb0f19cbb2ecf
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-12-17 07:54:29 +0300

    hometask_2: humanizeDiff for positive numbers

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -22,7 +22,7 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
         TimeUnits.HOUR -> value * HOUR
         TimeUnits.DAY -> value * DAY
     }
-
+    this.time = time
     return this
 }
 
@@ -30,9 +30,30 @@ enum class TimeUnits {
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY;
+
+    fun plural(value: Long): String {
+        when (value % 10L) {
+            0L -> return "минут"
+            1L -> return "минуту"
+            in 2L..4L -> return "минуты"
+            in 5L..9L -> return "минут"
+        }
+
+        return ""
+    }
 }
 
 fun Date.humanizeDiff(date:Date = Date()): String {
-    return ""
+    return when(val diff = date.time - this.time) {
+        in 0..1*SECOND -> "только что"
+        in 1*SECOND..45*SECOND -> "несколько секунд назад"
+        in 45*SECOND..75*SECOND -> "минуту назад"
+        in 75*SECOND..45*MINUTE -> "${diff / MINUTE} ${TimeUnits.MINUTE.plural(diff / MINUTE)} назад"
+        in 45*MINUTE..75*MINUTE -> "час назад"
+        in 75*MINUTE..22*HOUR -> "${diff / HOUR} часов назад"
+        in 22*HOUR..26*HOUR -> "день назад"
+        in 26*HOUR..360*DAY -> "${diff / DAY} дней назад"
+        else -> if (diff > 0) "более года назад" else "более, чем через год"
+    }
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -7,10 +7,56 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        return ""
+        var res: String = ""
+        for (c in payload) {
+            res += when(c) {
+                'а' -> 'a'
+                'б' -> 'b'
+                'в' -> 'v'
+                'г' -> 'g'
+                'д' -> 'd'
+                'е' -> 'e'
+                'ё' -> 'e'
+                'ж' -> "zh"
+                'з' -> 'z'
+                'и' -> 'i'
+                'й' -> 'i'
+                'к' -> 'k'
+                'л' -> 'l'
+                'м' -> 'm'
+                'н' -> 'n'
+                'о' -> 'o'
+                'п' -> 'p'
+                'р' -> 'r'
+                'с' -> 's'
+                'т' -> 't'
+                'у' -> 'u'
+                'ф' -> 'f'
+                'х' -> 'h'
+                'ц' -> 'c'
+                'ч' -> "ch"
+                'ш' -> "sh"
+                'щ' -> "sh"
+                'ъ' -> ""
+                'ы' -> 'i'
+                'ь' -> ""
+                'э' -> 'e'
+                'ю' -> "yu"
+                'я' -> "ya"
+                ' ' -> divider
+                else -> c
+            }
+        }
+        return res
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        return ""
+        //return firstName[0].toString().toUpperCase() + lastName[0].toString().toUpperCase()
+        if (firstName.isNullOrEmpty() && (lastName.isNullOrEmpty())) return "null"
+        if (firstName.isNullOrEmpty()) return lastName!![0].toString().toUpperCase()
+        if (lastName.isNullOrEmpty()) return firstName[0].toString().toUpperCase()
+        return firstName[0].toString().toUpperCase() + lastName[0].toString().toUpperCase()
     }
+
+
 }
